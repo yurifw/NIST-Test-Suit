2d9217a606114bb46d38a412d981edade5302072
yurifw <yuridefw2@gmail.com>
2015-07-17 20:48:40 -0300

    Teste11 alterado para ser executado em vários arquivos

--- src/controle/ControleTeste11.java
+++ src/controle/ControleTeste11.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package controle;
 
 import modelo.Teste11;
@@ -14,41 +13,43 @@ import org.apache.commons.math.MathException;
  * @author renatohidaka
  */
 public class ControleTeste11 extends ControleGenerico {
-    
+
     private final Teste11 teste = new Teste11();
- 
+
     public String run(String arquivo, String bloco) {
 
-        Integer[] e = validarArquivoEntrada(arquivo);
-        boolean saida;
-        int M;
-        
-        if (e != null) {
-            try {
+        Integer[][] e = validarEntrada(arquivo);
+        for (int i = 0; i < e.length; i++) {
+            boolean saida;
+            int M;
 
-                M = Integer.parseInt(bloco);
+            if (e[i] != null) {
+                try {
 
-            } catch (NumberFormatException ex) {
-                return "Block length must to be filled with numbers!";
-            }
+                    M = Integer.parseInt(bloco);
 
-            if (M < 3) {
-                return "Block length must be greater or equal to 3!";
-            }
+                } catch (NumberFormatException ex) {
+                    return "Block length must to be filled with numbers!";
+                }
 
-            try {
-                saida = teste.run(e, M);
-                return (saida == true ? "Random sequence!" : "Not random sequence!").concat("\np_value1=" + teste.getValor_p1()).
-                        concat("\np_value2="+teste.getValor_p2());
+                if (M < 3) {
+                    return "Block length must be greater or equal to 3!";
+                }
 
-            } catch (MathException ex) {
-                return "Error, try again!";
-            } catch(OutOfMemoryError om) {
-                return "Out of memory error!";
+                try {
+                    saida = teste.run(e, M);
+                    return (saida == true ? "Random sequence!" : "Not random sequence!").concat("\np_value1=" + teste.getValor_p1()).
+                            concat("\np_value2=" + teste.getValor_p2());
+
+                } catch (MathException ex) {
+                    return "Error, try again!";
+                } catch (OutOfMemoryError om) {
+                    return "Out of memory error!";
+                }
             }
         }
 
         return msgErro;
     }
-    
+
 }
--- src/modelo/Teste11.java
+++ src/modelo/Teste11.java
@@ -29,64 +29,69 @@ public class Teste11 {
     private double valor_p2;
     private static final double ALFA = 0.01;
 
-    public boolean run(Integer[] temp, int m) throws MathException {
+    public boolean run(Integer[][] temp, int m) throws MathException {
+        valor_p1 = 0;
+        valor_p2 = 0;
+        for (int l = 0; l < temp.length; l++) {
 
-        Integer[] e = new Integer[temp.length + m - 1];
-        phi = new double[m];
-        n = temp.length;
-        Arrays.fill(e,0);
-        System.arraycopy(temp, 0, e, 0, temp.length);
-        tabelas.clear();
+            Integer[] e = new Integer[temp[l].length + m - 1];
+            phi = new double[m];
+            n = temp[l].length;
+            Arrays.fill(e, 0);
+            System.arraycopy(temp[l], 0, e, 0, temp[l].length);
+            tabelas.clear();
 
-        for (int i = m; i > 0; i--) {
+            for (int i = m; i > 0; i--) {
 
-            Lbit = new HashMap<String, Integer>();
-            montaTabela(i);
+                Lbit = new HashMap<String, Integer>();
+                montaTabela(i);
 
-            for (int j = 0; j <= e.length - m; j++) {//se for para considerar somente até a sequencia verdadeira usamos o (-m) caso contrário (-i)
+                for (int j = 0; j <= e.length - m; j++) {//se for para considerar somente até a sequencia verdadeira usamos o (-m) caso contrário (-i)
 
-                String str = "";
-                for (int k = j; k < i + j; k++) {
+                    String str = "";
+                    for (int k = j; k < i + j; k++) {
 
-                    str = str.concat(String.valueOf(e[k]));
+                        str = str.concat(String.valueOf(e[k]));
+                    }
+
+                    Lbit.put(str, Lbit.get(str) + 1);
+
                 }
 
-                Lbit.put(str, Lbit.get(str) + 1);
+                tabelas.add(Lbit);
 
             }
 
-            tabelas.add(Lbit);
+            int k = m;
+            int soma = 0;
+            int pont = 0;
 
-        }
+            for (HashMap<String, Integer> valor : tabelas) {
 
+                TreeSet<String> tree = new TreeSet<String>(valor.keySet());
+                soma = 0;
+                for (String string : tree) {
 
-        int k = m;
-        int soma = 0;
-        int pont = 0;
+                    soma += Math.pow(valor.get(string), 2);
+                }
 
-        for (HashMap<String, Integer> valor : tabelas) {
-
-            TreeSet<String> tree = new TreeSet<String>(valor.keySet());
-            soma = 0;
-            for (String string : tree) {
-
-                soma += Math.pow(valor.get(string), 2);
+                phi[pont] = ((Math.pow(2, k) / n) * soma) - n;
+                k--;
+                pont++;
             }
 
-            phi[pont] = ((Math.pow(2, k) / n) * soma) - n;
-            k--;
-            pont++;
+            dfp = phi[phi.length - m] - phi[phi.length - m + 1];
+            dfs = phi[phi.length - m] - (2 * phi[phi.length - m + 1]) + phi[phi.length - m + 2];
+
+            gama = new GammaDistributionImpl(Math.abs(dfp / 2d), 1);
+            valor_p1 += 1 - gama.cumulativeProbability(Math.pow(2, m - 2));
+
+            gama = new GammaDistributionImpl(Math.abs(dfp / 2d), 1);
+            valor_p2 += 1 - gama.cumulativeProbability(Math.pow(2, m - 3));
         }
+        valor_p1 = valor_p1 / temp.length;
+        valor_p2 = valor_p2 / temp.length;
 
-        dfp = phi[phi.length-m] - phi[phi.length - m + 1];
-        dfs = phi[phi.length-m] - (2*phi[phi.length - m + 1]) + phi[phi.length - m + 2];
-
-        gama = new GammaDistributionImpl(Math.abs(dfp/2d), 1);
-        valor_p1 = 1 - gama.cumulativeProbability(Math.pow(2, m-2));
-
-        gama = new GammaDistributionImpl(Math.abs(dfp/2d), 1);
-        valor_p2 = 1 - gama.cumulativeProbability(Math.pow(2, m-3));
-      
         return valor_p1 > ALFA && valor_p2 > ALFA;
     }
 
